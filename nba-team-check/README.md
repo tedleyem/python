fc1833b469564b4a946f63d942de9b660efaf436
tmeralus <tmeralus@gmail.com>
2023-06-24 19:38:20 -0400

    added text

---
+++ nba-team-check/README.md
@@ -0,0 +1,5 @@
+# NBA TEAM CHECK 
+Python script to scrape nba player data 
+
+Helpful Resources 
+https://pypi.org/project/nba-history/
---
+++ nba-team-check/teams-check.py
@@ -0,0 +1,17 @@
+import nba_history
+
+from nba_history import player_data 
+
+def scrape_draft_data():
+
+def scrape_player_salaries():
+
+def scrape_player_total_stats():
+
+def scrape_player_per_game_stats():
+
+def scrape_player_shooting_stats():
+
+def scrape_all_stars():
+
+def scrape_nba_team_records():
