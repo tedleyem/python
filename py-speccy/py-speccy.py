d208cd5a54fd9f1ca2b500e4a32d9f726184dc2a
Tedley Meralus <tmeralus@gmail.com>
2024-11-01 16:55:10 -0400

    reordered output statements

--- py-speccy/py-speccy.py
+++ py-speccy/py-speccy.py
@@ -1,7 +1,5 @@
-import psutil
 import platform
 from datetime import datetime
-import cpuinfo
 import socket
 import uuid
 import re
@@ -19,66 +17,46 @@ def get_size(bytes, suffix="B"):
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= size_factor
 
-def System_information():
-    print("="*10, "System Information", "="*10)
-    uname = platform.uname()
-    print(f"System: {uname.system}")
-    print(f"Node Name: {uname.node}")
-    print(f"Release: {uname.release}")
-    print(f"Version: {uname.version}")
-    print(f"Machine: {uname.machine}")
-    print(f"Processor: {uname.processor}")
-    print(f"Processor: {cpuinfo.get_cpu_info()['brand_raw']}")
-
+def ram_stats():
     # Memory Information
-    print("="*10, "Memory Information", "="*10)
-    # get memory stats
-    svmem = psutil.virtual_memory()
-    print(f"Total: {get_size(svmem.total)}")
-    print(f"Available: {get_size(svmem.available)}")
-    print(f"Used: {get_size(svmem.used)}")
-    print(f"Percentage: {svmem.percent}%")
+    print("="*10, "Memory Information", "="*10) 
 
+def cpu_stats():
     # print CPU information
-    print("="*10, "CPU Info", "="*10)
-    # number of cores
-    print("Physical cores:", psutil.cpu_count(logical=False))
-    print("Total cores:", psutil.cpu_count(logical=True))
-    # CPU frequencies
-    cpufreq = psutil.cpu_freq()
-    print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
-    print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
-    print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
-    print(f"Total CPU Usage: {psutil.cpu_percent()}%")
-    # CPU usage
-    #print("CPU Usage Per Core:")
-    #for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
-    #    print(f"Core {i}: {percentage}%")
+    print("="*10, "CPU Info", "="*10) 
 
+def disk_stats():
     # Disk Information
     print("="*10, "Disk Information", "="*10)
-    print("Partitions and Usage:")
-    # get all disk partitions
-    partitions = psutil.disk_partitions()
-    for partition in partitions:
-        print(f"=== Device: {partition.device} ===")
-        print(f"  Mountpoint: {partition.mountpoint}")
-        print(f"  File system type: {partition.fstype}")
-        try:
-            partition_usage = psutil.disk_usage(partition.mountpoint)
-        except PermissionError:
-            # this can be catched due to the disk that
-            # isn't ready
-            continue
-        print(f"  Total Size: {get_size(partition_usage.total)}")
-        print(f"  Used: {get_size(partition_usage.used)}")
-        print(f"  Free: {get_size(partition_usage.free)}")
-        print(f"  Percentage: {partition_usage.percent}%")
-    # get IO statistics since boot
-    disk_io = psutil.disk_io_counters()
-    print(f"Total read: {get_size(disk_io.read_bytes)}")
-    print(f"Total write: {get_size(disk_io.write_bytes)}")
+    print("Partitions and Usage:") 
+     
+def more_stats():
+    cpu_stats()
+    disk_stats()
+    ram_stats()
+
+def System_information():
+    print("="*10, "System Information", "="*10)
+    print(f"user@hostname")
+    uname = platform.uname()
+    print(f"OS: {uname.system} {uname.release} {uname.machine}")
+    print(f"Host: {uname.node}") 
+    print(f"Kernel: test")
+    print(f"Uptime: test")
+    print(f"Memory: test") 
+    print(f"Packages: test")
+    print(f"Shell: test")  
+    #more_stats()
+    print(f"CPU: test")
+    print(f"RAM: test")
+    print(f"Motherboard: test")
+    print(f"Graphics: test")
+    print(f"Storage: test")
+    print(f"Optical Drives: test")
+    print(f"Audio: test")
+    print(f"")
+    #print(f"Tip: --full for full system specs")
+    #print(f"Tip: --h for help")
 
 if __name__ == "__main__":
-
     System_information()
